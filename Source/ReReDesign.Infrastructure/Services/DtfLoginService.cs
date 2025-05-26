1d125efef466e9f5767a18fbe7a75ed61a31ed72
Poklonsky Alex <Aleksey.Poklonsky@infotecs.ru>
2025-05-26 11:35:22 +0300

    feat: cache login result.

--- Source/ReReDesign.Infrastructure/Services/DtfLoginService.cs
+++ Source/ReReDesign.Infrastructure/Services/DtfLoginService.cs
@@ -3,14 +3,23 @@
 /// <inheritdoc />
 public class DtfLoginService(
     ILogger<DtfLoginService> logger,
-    IOptions<DtfLoginSettings> loginOptions) : IDtfLoginService
+    IOptions<DtfLoginSettings> loginOptions,
+    IMemoryCache memoryCache) : IDtfLoginService
 {
-    const string? LoginUrl = "https://api.dtf.ru/v3.4/auth/email/login";
+    private const string LoginUrl = "https://api.dtf.ru/v3.4/auth/email/login";
+    private const string CacheKey = "Login";
     private DtfLoginSettings DtfLoginSettings => loginOptions.Value;
+    private readonly MemoryCacheProvider provider = new(memoryCache);
 
     /// <inheritdoc />
     public async Task<DtfLoginResult> Login(CancellationToken cancellation)
     {
+        if (provider.TryGet(CacheKey) is (true, DtfLoginResult cachedLoginResult))
+        {
+            logger.LogDebug("Cache hit!");
+            return cachedLoginResult;
+        }
+
         HttpClient httpClient = new();
 
         MultipartFormDataContent formData = new("----WebKitFormBoundary8L5jnfclCLQ5y9t5");
@@ -26,12 +35,27 @@ public class DtfLoginService(
             response.EnsureSuccessStatusCode();
 
             string responseBody = await response.Content.ReadAsStringAsync(cancellation);
-            return Newtonsoft.Json.JsonConvert.DeserializeObject<DtfLoginResult>(responseBody);
+            DtfLoginResult result = Newtonsoft.Json.JsonConvert.DeserializeObject<DtfLoginResult>(responseBody);
+            provider.Put(CacheKey, result, new(GetTtl(result.Data)));
+            return result;
         }
         catch (HttpRequestException ex)
         {
             logger.LogError(ex, "Can't login");
             throw;
         }
+    }
+
+    private static TimeSpan GetTtl(DtfLoginData loginData)
+    {
+        DateTime expirationTime = DateTimeOffset.FromUnixTimeSeconds(loginData.AccessExpTimestamp).UtcDateTime;
+        DateTime now = DateTime.UtcNow;
+
+        TimeSpan ttl = expirationTime - now;
+
+        if (ttl < TimeSpan.Zero)
+            ttl = TimeSpan.Zero;
+
+        return ttl;
     }
 }
