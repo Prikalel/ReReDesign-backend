091c9727787a5fc2aa36b646d20d7b7f64d91fc5
Poklonsky Alex <Aleksey.Poklonsky@infotecs.ru>
2025-05-26 10:31:15 +0300

    feat: add login service.

---
+++ Source/ReReDesign.Infrastructure/Services/DtfLoginService.cs
@@ -0,0 +1,37 @@
+﻿namespace VelikiyPrikalel.ReReDesign.Infrastructure.Services;
+
+/// <inheritdoc />
+public class DtfLoginService(
+    ILogger<DtfLoginService> logger,
+    IOptions<DtfLoginSettings> loginOptions) : IDtfLoginService
+{
+    const string? LoginUrl = "https://api.dtf.ru/v3.4/auth/email/login";
+    private DtfLoginSettings DtfLoginSettings => loginOptions.Value;
+
+    /// <inheritdoc />
+    public async Task<DtfLoginResult> Login(CancellationToken cancellation)
+    {
+        HttpClient httpClient = new();
+
+        MultipartFormDataContent formData = new("----WebKitFormBoundary8L5jnfclCLQ5y9t5");
+
+        formData.Add(new StringContent(DtfLoginSettings.Email), "email");
+
+        formData.Add(new StringContent(DtfLoginSettings.Password), "password");
+
+        try
+        {
+            HttpResponseMessage response = await httpClient.PostAsync(LoginUrl, formData, cancellation);
+
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = await response.Content.ReadAsStringAsync(cancellation);
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<DtfLoginResult>(responseBody);
+        }
+        catch (HttpRequestException ex)
+        {
+            logger.LogError(ex, "Can't login");
+            throw;
+        }
+    }
+}
